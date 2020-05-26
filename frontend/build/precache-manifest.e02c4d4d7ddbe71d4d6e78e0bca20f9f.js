a80701b107070cec19cc74fe0239f82c47b607d7
mustafaabdullahk <mabdkus@hotmail.com>
2020-05-26 19:15:54 +0300

    4rd commit

---
+++ frontend/build/precache-manifest.e02c4d4d7ddbe71d4d6e78e0bca20f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac83c701d1412c36b62d4ed0b05b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abde4834279291f40912",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "206a49a7bb0d26c1e8c6",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "abde4834279291f40912",
+    "url": "/static/js/2.13131f78.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.13131f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206a49a7bb0d26c1e8c6",
+    "url": "/static/js/main.844cf0d8.chunk.js"
+  },
+  {
+    "revision": "b7893f4e0c490fed8154",
+    "url": "/static/js/runtime-main.762b7f25.js"
+  }
+]);
